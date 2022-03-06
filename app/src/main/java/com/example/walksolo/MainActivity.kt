12a0b25b97c8d3b7a4fb60e10788bcb5b38f26b5
tashmanr <rebeccatashman@gmail.com>
2022-03-06 17:46:17 +0200

    PROJ-16 ccessible buttons, talkback enabled and working

--- app/src/main/java/com/example/walksolo/MainActivity.kt
+++ app/src/main/java/com/example/walksolo/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.walksolo
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var navigateButton: Button
+    private lateinit var aroundMeButton: Button
+    private lateinit var layout: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navigateButton = findViewById(R.id.navigate)
+        navigateButton.setOnClickListener(this)
+        aroundMeButton = findViewById(R.id.aroundme)
+        aroundMeButton.setOnClickListener(this)
+        layout = findViewById(R.id.coordinatorLayout)
+    }
+
+    //function that waits for connect button to be pressed when pressed will tell viewmodel to tell model to attempt to open a socket and connect to FlightGear
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.navigate -> {
+                //open up google maps options
+                showErrorMessage("Need to open maps!")
+            }
+            R.id.aroundme -> {
+                showErrorMessage("Please tell me what's around me.")
+            }
+        }
+    }
+
+    //function for showing the appropriate error message depending upon the error
+    @SuppressLint("ShowToast")
+    fun showErrorMessage(s: String) {
+        Snackbar.make(layout, s, Snackbar.LENGTH_SHORT)
+            .setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_FADE)
+            .setBackgroundTint(Color.GRAY).show()
     }
 }
