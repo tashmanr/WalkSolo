88f2ab1ea5c1bf6cd2425754992104c61323619e
LolaSi <lola.siross@gmail.com>
2022-03-31 19:08:31 +0300

    Lola - changed target SDK to 30, updated bluetooth permissions

--- app/src/main/java/com/example/walksolo/MainActivity.kt
+++ app/src/main/java/com/example/walksolo/MainActivity.kt
@@ -9,13 +9,15 @@ import android.view.View
 import android.widget.Button
 import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
-
+import android.app.Activity
 import android.os.Build
 import androidx.core.content.ContextCompat
 import PermissionUtils.PermissionDeniedDialog.Companion.newInstance
 import PermissionUtils.isPermissionGranted
 import PermissionUtils.requestPermission
 import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
 import android.content.pm.PackageManager
 import android.widget.Toast
 //import android.bluetooth.BluetoothManager
@@ -29,10 +31,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private var btScanGranted: Boolean = false
     private var btConnectGranted: Boolean = false
 
+    private var m_bluetoothAdapter: BluetoothAdapter ?= null
+
     companion object{
-        private const val BT_SCAN_REQUEST_CODE = 100
-        private const val BT_CONNECT_REQUEST_CODE = 101
+        private const val REQUEST_ENABLE_BT = 1
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -52,99 +56,43 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 startActivity(navigateIntent)
             }
             R.id.aroundme -> {
+                showErrorMessage("going to enableBT")
                 enableBluetooth()
-                /*
-                var bluetoothManager:BluetoothManager = this.getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
-                var bluetoothAdapter: BluetoothAdapter = bluetoothManager.adapter
-                var outp:String = bluetoothAdapter.name
-                Toast.makeText(this,outp,Toast.LENGTH_SHORT).show(
-                showErrorMessage(outp)*/
-                if (btConnectGranted && btScanGranted){
-                    showErrorMessage("btConnect and btScan Granted")
-                }
-                else if (btConnectGranted){
-                    showErrorMessage("only connect granted")
-                }
-                else if (btScanGranted){
-                    showErrorMessage("only scan granted")
-                }
-                else{
-                    showErrorMessage("nothing was granted")
-                }
             }
         }
     }
 
-    /**
-     * Enables if the BT_SCAN permission has been granted.
-     */
     @SuppressLint("MissingPermission")
-    private fun enableBluetooth() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN)
-            == PackageManager.PERMISSION_GRANTED
-        ) {
-            Toast.makeText(this,"permission BT SCAN is enabled",Toast.LENGTH_SHORT).show()
-        } else {
-            // Permission get bluetooth scan
-            requestPermission(
-                this, BT_SCAN_REQUEST_CODE,
-                Manifest.permission.BLUETOOTH_SCAN, true
-            )
-        }
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT)
-            == PackageManager.PERMISSION_GRANTED
-        ) {
-            Toast.makeText(this,"permission BT CONNECT is enabled",Toast.LENGTH_SHORT).show()
-        } else {
-            // Permission to Bluetooth_CONNECT
-            requestPermission(
-                this, BT_CONNECT_REQUEST_CODE,
-                Manifest.permission.BLUETOOTH_CONNECT, true
-            )
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        if (requestCode == BT_SCAN_REQUEST_CODE){
-            if (isPermissionGranted(
-                    permissions,
-                    grantResults,
-                    Manifest.permission.BLUETOOTH_SCAN
-                )
-            ) {
-                // the permission has been granted.
-                btScanGranted = true
-                Toast.makeText(this,"permission BT SCAN is enabled",Toast.LENGTH_SHORT).show()
-            } else {
-                // Permission was denied. Display an error message
-                Toast.makeText(this,"permission BT SCAN DENIED",Toast.LENGTH_SHORT).show()
-            }
-        }
-        else if(requestCode == BT_CONNECT_REQUEST_CODE){
-            if (isPermissionGranted(
-                    permissions,
-                    grantResults,
-                    Manifest.permission.BLUETOOTH_CONNECT
-                )
-            ) {
-                // the permission has been granted.
-                btConnectGranted = true
-                Toast.makeText(this,"permission BT CONNECT is enabled",Toast.LENGTH_SHORT).show()
-            } else {
-                // Permission was denied. Display an error message
-                Toast.makeText(this,"permission BT CONNECT DENIED",Toast.LENGTH_SHORT).show()
-            }
-        }
-        else{
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    private fun enableBluetooth(){
+        showErrorMessage("in enableBT")
+        m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if(m_bluetoothAdapter == null) {
+            showErrorMessage("this device doesn't support bluetooth")
             return
         }
+        if(!m_bluetoothAdapter!!.isEnabled) {
+            showErrorMessage("second if 222222")
+            showErrorMessage("going to ask permission")
+            val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BT)
+        }
     }
 
+    @SuppressLint("MissingPermission")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (m_bluetoothAdapter!!.isEnabled) {
+                    showErrorMessage("Bluetooth has been enabled")
+                } else {
+                    showErrorMessage("Bluetooth has been disabled")
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                showErrorMessage("Bluetooth enabling has been canceled")
+            }
+        }
+    }
 
     //function for showing the appropriate error message depending upon the error
     @SuppressLint("ShowToast")
