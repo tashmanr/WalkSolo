52bf5dd15482f81099c8464aea1af8bb76926665
rebecca <rebeccat@gojuno.com>
2022-06-16 20:24:57 +0300

    feat: GoogleDirectionsAPIHandler.kt

---
+++ app/src/main/java/com/example/walksolo/GoogleDirectionsAPIHandler.kt
@@ -0,0 +1,34 @@
+package com.example.walksolo
+
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.net.URL
+
+
+class GoogleDirectionsAPIHandler {
+    fun getDirections(origin: String, destination: String) {
+        // Create OkHttp Client
+        val client: OkHttpClient = OkHttpClient()
+        val result = mapsHttpRequest(getRequestUrl(origin, destination), client)
+    }
+
+    private fun mapsHttpRequest(sUrl: String, client: OkHttpClient): String? {
+        var result: String? = null
+        try {
+            // Create URL
+            val url = URL(sUrl)
+            // Build request
+            val request = Request.Builder().url(url).build()
+            // Execute request
+            val response = client.newCall(request).execute()
+            result = response.body?.string()
+        } catch (err: Error) {
+            print("Error when executing get request: " + err.localizedMessage)
+        }
+        return result
+    }
+
+    private fun getRequestUrl(origin: String, destination: String): String {
+        return "https://maps.googleapis.com/maps/api/directions/json?origin=$origin&destination=$destination&mode=walking&key=${BuildConfig.MAPS_API_KEY}"
+    }
+}
--- app/src/main/java/com/example/walksolo/MapsActivity.kt
+++ app/src/main/java/com/example/walksolo/MapsActivity.kt
@@ -6,16 +6,10 @@ import PermissionUtils.requestPermission
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
-import android.location.Location
 import android.os.Bundle
-import android.widget.Toast
+import android.os.StrictMode
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
-import com.example.walksolo.BuildConfig.MAPS_API_KEY
 import com.example.walksolo.databinding.ActivityMapsBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
@@ -25,23 +19,15 @@ import com.google.android.gms.location.LocationSettingsRequest
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
-import org.json.JSONObject
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
-    GoogleMap.OnMarkerClickListener
-//    GoogleMap.OnMyLocationButtonClickListener,
-//    GoogleMap.OnMyLocationClickListener,
-//ActivityCompat.OnRequestPermissionsResultCallback {
-{
+    GoogleMap.OnMarkerClickListener {
     private lateinit var map: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     private var permissionDenied = false
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var lastLocation: Location
     private var locationRequest: LocationRequest? = null
     private val UPDATE_INTERVAL = (10 * 1000 /* 10 secs */).toLong()
     private val FASTEST_INTERVAL: Long = 2000 /* 2 sec */
@@ -56,6 +42,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
         fusedLocationClient = getFusedLocationProviderClient(this)
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+        GoogleDirectionsAPIHandler().getDirections("The White House", "Capital Hill")
     }
 
     // Trigger new location updates at interval
@@ -75,27 +64,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
         // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient
         val settingsClient = LocationServices.getSettingsClient(this)
         settingsClient.checkLocationSettings(locationSettingsRequest)
-
-        // new Google API SDK v11 uses getFusedLocationProviderClient(this)
-//        getFusedLocationProviderClient(this).requestLocationUpdates(
-//            locationRequest, object : LocationCallback() {
-//                override fun onLocationResult(locationResult: LocationResult) {
-//                    // do work here
-//                    onLocationChanged(locationResult.lastLocation)
-//                }
-//            },
-//            Looper.myLooper()
-//        )
-    }
-
-    private fun onLocationChanged(location: Location) {
-        // New location has now been determined
-        val msg = "Updated Location: " +
-                java.lang.Double.toString(location.latitude) + "," +
-                java.lang.Double.toString(location.longitude)
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
-        // You can now create a LatLng Object for use with maps
-        val latLng = LatLng(location.latitude, location.longitude)
     }
 
     /**
@@ -107,13 +75,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
      */
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-//        googleMap.setOnMyLocationButtonClickListener(this)
-//        googleMap.setOnMyLocationClickListener(this)
-//        val latLngOrigin = "10.3181466,123.9029382"
-//        val latLngDestination = "10.311795,123.915864"
-////        val path: MutableList<List<LatLng>> = ArrayList()
-//        val urlDirections = getRequestUrl(latLngOrigin, latLngDestination, "walking")
-//        mapsHttpRequest("GET", urlDirections)
         map.uiSettings.isZoomControlsEnabled = true
         map.setOnMarkerClickListener(this)
         enableMyLocation()
@@ -138,31 +99,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
                 Manifest.permission.ACCESS_FINE_LOCATION, true
             )
         }
-//        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
-//            if (location != null) {
-//                lastLocation = location
-//                val currentLatLng = LatLng(location.latitude, location.longitude)
-//                //createMarker(currentLatLng)
-//                //map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
-//            }
-//        }
     }
-
-    private fun createMarker(location: LatLng) {
-        val markerOptions = MarkerOptions().position(location)
-        map.addMarker(markerOptions)
-    }
-
-//    override fun onMyLocationButtonClick(): Boolean {
-//        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show()
-//        // Return false so that we don't consume the event and the default behavior still occurs
-//        // (the camera animates to the user's current position).
-//        return false
-//    }
-//
-//    override fun onMyLocationClick(location: Location) {
-//        Toast.makeText(this, "Current location:\n$location", Toast.LENGTH_LONG).show()
-//    }
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -211,44 +148,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
          * @see .onRequestPermissionsResult
          */
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1
-    }
-
-    private fun mapsHttpRequest(type: String, url: String) {
-        var method = Request.Method.POST
-        if (type == "GET") {
-            method = Request.Method.GET
-        } else if (type == "POST") {
-            method = Request.Method.POST
-        }
-        val request = object :
-            StringRequest(method, url, Response.Listener<String> { response ->
-                val jsonResponse = JSONObject(response)
-                // Get routes
-                val routes = jsonResponse.getJSONArray("routes")
-                val legs = routes.getJSONObject(0).getJSONArray("legs")
-                val steps = legs.getJSONObject(0).getJSONArray("steps")
-//            for (i in 0 until steps.length()) {
-//                val points = steps.getJSONObject(i).getJSONObject("polyline").getString("points")
-//                path.add(PolyUtil.decode(points))
-//            }
-//            for (i in 0 until path.size) {
-//                this.map!!.addPolyline(PolylineOptions().addAll(path[i]).color(Color.RED))
-//            }
-            }, Response.ErrorListener { _ ->
-            }) {}
-        val requestQueue = Volley.newRequestQueue(this)
-        requestQueue.add(request)
-    }
-
-//    private fun getMyLocation(): String {
-//        mapsHttpRequest(
-//            "POST",
-//            "https://www.googleapis.com/geolocation/v1/geolocate?key=$MAPS_API_KEY"
-//        )
-//    }
-
-    private fun getRequestUrl(origin: String, destination: String, mode: String): String {
-        return "https://maps.googleapis.com/maps/api/directions/json?origin=$origin&destination=$destination&mode=$mode&key=$MAPS_API_KEY"
     }
 
     override fun onMarkerClick(p0: Marker?): Boolean {
