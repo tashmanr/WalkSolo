229e9999e1d2f5205fe82bae3b1eaaa187e0e3ff
LolaSi <lola.siross@gmail.com>
2022-06-23 10:57:27 +0300

    handle no vision response

--- app/src/main/java/com/example/walksolo/VisionsResponseHandler.kt
+++ app/src/main/java/com/example/walksolo/VisionsResponseHandler.kt
@@ -7,20 +7,23 @@ import java.util.*
 
 class VisionsResponseHandler {
     fun processResponse(responses: BatchAnnotateImagesResponse): String {
-        val responseList: ArrayList<Objects> = responses["responses"] as ArrayList<Objects>
-        //if nothing was identified
-        if (responseList.size < 1) {
-            return "No blockade"
-        }
-        val annotateImagesResponse = responseList[0] as AnnotateImageResponse
-        val objectAnnotations = annotateImagesResponse["localizedObjectAnnotations"]
-        if (objectAnnotations != null) {
-            //if something was identified
-            if ((objectAnnotations as List<*>).size >= 1) {
-                //TODO: for now return first one, later add logic to get biggest blockade
-                return ((objectAnnotations as List<*>).first() as ArrayMap<*, *>)["name"].toString()
+        if (responses["responses"] != null) {
+            val responseList: ArrayList<Objects> = responses["responses"] as ArrayList<Objects>
+            //if nothing was identified
+            if (responseList.size < 1) {
+                return "No blockade"
+            }
+            val annotateImagesResponse = responseList[0] as AnnotateImageResponse
+            val objectAnnotations = annotateImagesResponse["localizedObjectAnnotations"]
+            if (objectAnnotations != null) {
+                //if something was identified
+                if ((objectAnnotations as List<*>).size >= 1) {
+                    //TODO: for now return first one, later add logic to get biggest blockade
+                    return ((objectAnnotations as List<*>).first() as ArrayMap<*, *>)["name"].toString()
+                }
             }
         }
+
         //else nothing was identified
         return "No blockade"
     }
