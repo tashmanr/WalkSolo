eb05db5b0c288119ccf4969df24a487c8c350754
LolaSi <lola.siross@gmail.com>
2022-07-27 18:36:12 +0300

    Lola - code cleanup

--- app/src/main/java/com/example/walksolo/BluetoothService.kt
+++ app/src/main/java/com/example/walksolo/BluetoothService.kt
@@ -1,5 +1,21 @@
 package com.example.walksolo
 
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
@@ -295,7 +311,6 @@ class BluetoothService(handler: Handler) {
         override fun run() {
             Log.i(TAG, "BEGIN mConnectedThread")
             var buffer: ByteArray = ByteArray(0)
-            var bytes: Int
             var newMsg: Boolean = true
             var bytesToReceive: Int = 0
             var bytesReceived: Int = 0
